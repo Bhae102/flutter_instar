298a98afd427333840b2a41c1d2189e924f75a61
Bhae <18reggae@gmail.com>
2020-11-04 15:19:09 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/real102/instargarm_clon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.real102.instargarm_clon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instargarm_clon/tab_page.dart';
+
+class RootPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return TapPage();
+  }
+}
+
